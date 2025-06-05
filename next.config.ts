e5adadd27230892b0ef360a3a8849aab13227e18
Victor Ivan <victor.simoes@allos.co>
2025-06-05 20:45:33 -0300

    ci: fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  compiler: {
+    define: {
+      "@/": "/src",
+    },
+  }
 };
 
 export default nextConfig;
