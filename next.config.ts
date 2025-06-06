3bca937d109687c6c3e6615f37c48d148aa7b546
Victor Ivan <victor.simoes@allos.co>
2025-06-06 08:01:34 -0300

    ci: trying to fix

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  compiler: {
-    define: {
-      "@/": "/src",
-    },
-  }
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
